5ebe04958f96bff18298df4384b086d7e47b5c64
Leonid Vinzhega <leonidlvi@gmail.com>
2021-10-21 20:36:42 +0300

    Add routes

--- task-manager/app.js
+++ task-manager/app.js
@@ -5,11 +5,26 @@ const port = 3000
 
 app.use(express.static('./public'));
 
-
-app.get('/', (req, res) => {
-    res.send("Home");
+//routes
+app.get('/api/v1/tasks', (req, res) => {
+    res.send("Get All Tasks");
 })
 
+app.post('/api/v1/tasks', (req, res) => {
+    res.send("Create Task");
+})
+
+app.get('/api/v1/tasks/:id', (req, res) => {
+    res.send("Get Single Task");
+})
+
+app.patch('/api/v1/tasks/:id', (req, res) => {
+    res.send("Update Task");
+})
+
+app.delete('/api/v1/tasks/:id', (req, res) => {
+    res.send("Delete Task");
+})
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
