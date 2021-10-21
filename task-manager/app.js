9c76d69e4e67488d756662a0f19606ae0eaa89f6
Leonid Vinzhega <leonidlvi@gmail.com>
2021-10-21 23:01:52 +0300

    Added mongodb connection

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routers/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 const port = 3000
 
+//middleware
 app.use(express.static('./public'));
+app.use(express.urlencoded({extended: false}));
 
 //routes
 app.use('/api/v1/tasks', tasks);
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server is listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+start();
