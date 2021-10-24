c5bcece1a6603b67d8ec24cb9cd25f18bb6d3101
Leonid Vinzhega <leonidlvi@gmail.com>
2021-10-24 22:12:18 +0300

    minor Task schema fixex

--- task-manager/models/Tasks.js
+++ task-manager/models/Tasks.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     name: {
         type: String,
-        maxlength: 30,
-        required: true,
-        minlength: 2,
+        maxlength: [20, 'name can not be more than 20 character'],
+        required: [true, 'task must provide name'],
+        minlength: [2, 'minumue task name must be 2 character'],
         trim: true,
     },
     completed: {
