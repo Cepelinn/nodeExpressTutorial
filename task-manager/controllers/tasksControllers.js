78768f709cd32db35e328faa400f4b3d1cb6f192
Leonid Vinzhega <leonidlvi@gmail.com>
2021-10-25 21:26:53 +0300

    Added get, delete and update operartion

--- task-manager/controllers/tasksControllers.js
+++ task-manager/controllers/tasksControllers.js
@@ -1,24 +1,65 @@
 const Task = require('../models/Tasks')
 
-const getAllTasks = (req, res) => {
-    res.json(req.body);
+const getAllTasks = async (req, res) => {
+    try
+    {
+        const tasks = await Task.find({});
+        res.status(200).json({tasks});
+    } 
+    catch (error) 
+    {
+        res.status(500).json({msg: "can't get data from a database"});
+    }
 }
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body);
-    res.status(201).json({task})
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({task})
+    } catch (error) {
+        res.status(500).json({msg: "can't get data from a database"});
+    }
 }
 
-const getSingleTask = (req, res) => {
-    res.send('get single task');
+const getSingleTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.find({_id: taskID});
+        if(!task){
+            return res.status(404).json({msg:"Task not found"});
+        }
+        return res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({msg: "can't get data from a database"});
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send('update task');
+const updateTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
+
+        const task = await  Task.findOneAndUpdate({_id:taskID}, req.body, {
+            new: true,
+            runValidators: true,
+        })
+
+        return res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({msg:"can't reach database server"});
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete task');
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findByIdAndDelete(taskID);
+        if (!task) {
+            return res.status(404).json({mag:"Task not found"});
+        }
+        return res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({msg: "can't get data from a database"});
+    }
 }
 
 
