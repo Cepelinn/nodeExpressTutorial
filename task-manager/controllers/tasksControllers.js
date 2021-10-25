dfa6de5fde7c011127f4c09bb3e670a28534f987
Leonid Vinzhega <leonidlvi@gmail.com>
2021-10-25 21:38:18 +0300

    fix getSingleTask controller

--- task-manager/controllers/tasksControllers.js
+++ task-manager/controllers/tasksControllers.js
@@ -24,7 +24,7 @@ const createTask = async (req, res) => {
 const getSingleTask = async (req, res) => {
     try {
         const {id: taskID} = req.params;
-        const task = await Task.find({_id: taskID});
+        const task = await Task.findOne({_id: taskID});
         if(!task){
             return res.status(404).json({msg:"Task not found"});
         }
