e48a169f1ca999fea9a007dc2fad188469c99e45
Leonid Vinzhega <leonidlvi@gmail.com>
2021-11-01 23:29:37 +0300

    added sorting and pagination to get method"

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,9 +1,43 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({msg: 'Products testing route'});
+    const products = await Product.find({}).sort('name');
+    res.status(200).json({products, nbHits: products.length});
 };
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg:'Products route'});
+    const {featured, name, company, sort, fields} = req.query;
+    const queryObject = {};
+
+    if(featured){
+        queryObject.featured = featured === true ? true : false;
+    }
+    if(company){
+        queryObject.company = company;
+    }
+    if(name){
+        queryObject.name = {$regex:name, $options:'i'};
+    }
+    let result = Product.find(queryObject);
+
+    if (sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');
+    }
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+    const page = Number(req.query.page);
+    const limit = Number(req.query.limit);
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const products = await result;
+    res.status(200).json({products, nbHits:products.length});
 };
 
 module.exports = {
